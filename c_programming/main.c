6b5726ab65e9c36c8ea14b343867ae0465d787e8
pradhan7505 <pradhanprashish@gmail.com>
2020-10-12 09:31:49 +0545

    Initial Commit

---
+++ c_programming/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  c_programming
+//
+//  Created by Pradhan Prashish on 10/12/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
