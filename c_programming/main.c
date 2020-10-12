9c02356736e658d1680fd45c555b106511b54279
pradhan7505 <pradhanprashish@gmail.com>
2020-10-12 11:19:01 +0545

    commit for  first time

--- c_programming/main.c
+++ c_programming/main.c
@@ -1,14 +1,38 @@
-//
-//  main.c
-//  c_programming
-//
-//  Created by Pradhan Prashish on 10/12/20.
-//
-
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(){
+    
+    int num1,num2;
+    char operator;
+    
+    printf("Enter first number:");
+    scanf(" %d", &num1);
+    printf("Enter the operator:");
+    scanf(" %c", &operator);
+    printf("Enter second number:");
+    scanf(" %d", &num2);
+    
+    if(operator == '+')
+    {
+        printf("%d\n", num1+num2);
+    }
+    else if(operator == '-')
+    {
+        printf("%d\n", num1-num2);
+    }
+    else if (operator == '*')
+    {
+        printf("%c\n", num1*num2);
+    }
+    else if (operator == '/')
+    {
+        printf("%d\n", num1/num2);
+    }
+    else
+    {
+        printf("invalid command!!");
+    }
+    
+    
+    
     return 0;
 }
